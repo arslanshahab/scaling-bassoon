ded225567b8c76d377e6b216b786f5cba0a452f1
romesh-jaya <56665179+romesh-jaya@users.noreply.github.com>
2021-08-11 11:04:46 +0530

    fix: linting

--- components/language-switcher/availableTranslations.ts
+++ components/language-switcher/availableTranslations.ts
@@ -1,9 +1,6 @@
-import { INameCode } from './INameCode'
+import { NameCode } from '../../models/NameCode'
 
-// These are the translations the app supports,
-// the first will be used as default if none match the users browser preferences
-
-export const availableTranslations: INameCode[] = [
+export const availableTranslations: NameCode[] = [
   {
     name: 'EN',
     code: 'en',
---
+++ models/NameCode.ts
@@ -0,0 +1,4 @@
+export type NameCode = {
+  name: string
+  code: string
+}
