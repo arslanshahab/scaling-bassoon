05fb3221b4c3fd5fc8b5fbc5bf40983a6ec5c4a4
Romesh Jayawardene <romesh@creativehandles.com>
2021-08-18 04:21:47 +0000

    Merged in feature/update-build-scripts (pull request #15)
    Update build scripts
    
    * feat: update build scripts

--- deploy_staging.sh
+++
@@ -1,7 +0,0 @@
-# frontend deployement scripts
-cd /home/winmed-fe/staging
-rm -rf out
-npm ci
-npm run build
-
-echo 'Deployment to staging finished'
--- deploy_testing.sh
+++
@@ -1,7 +0,0 @@
-# frontend deployement scripts
-cd /home/winmed-fe/testing
-rm -rf out
-npm ci
-npm run build
-
-echo 'Deployment to testing finished'
