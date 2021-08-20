04b1b50dc0d7f92681f1f5293e568ee150191b0a
Arslan Shahab <arslan@creativehandles.com>
2021-08-20 11:01:25 +0000

    Merged in feature/14-about-us-homepage (pull request #22)
    Feature/14 about us homepage
    
    * added responsivness
    
    * added container in aboutus section
    
    * resolved merge conflicts
    
    * rearranged sections
    
    * replaced html button with Button component and added localization
    
    * removed .env by using rm --cached
    
    * changed alt text for about us section image
    
    * resolved merge conflicts
    
    * added link to about us button
    
    * Merge branch 'develop' of https://bitbucket.org/creativehandles/winmed-fe into feature/14-about-us-homepage
    
    
    Approved-by: Romesh Jayawardene

--- components/about-us/index.tsx
+++ components/about-us/index.tsx
@@ -1,10 +1,12 @@
 import useTranslation from 'next-translate/useTranslation'
+import { useRouter } from 'next/router'
 import ImageSection from '../image-section'
 import building from './../../assets/images/building-a.png'
 import styles from './AboutUs.module.scss'
 
 function AboutSection() {
   const { t } = useTranslation('common')
+  const router = useRouter()
   return (
     <div className={styles['about-section']}>
       <div className={styles.container}>
@@ -13,6 +15,7 @@ function AboutSection() {
           paragraphs={[t('loremIpsumLong')]}
           buttonText={t('aboutUs.titleCase')}
           image={building}
+          onClick={() => router.push('/aboutUs')}
         />
       </div>
     </div>
