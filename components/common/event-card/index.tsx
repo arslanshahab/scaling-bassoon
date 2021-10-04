0139205b21748b177ae0dd7b52b1e116a26ca038
Marcela Fikrlová <marcela@creativehandles.com>
2021-10-04 11:25:29 +0000

    Merged in feature/45-education-card (pull request #59)
    events page
    
    * fix prettier issue, change index to number
    
    * changes after cr
    
    * Merged creativehandles/winmed-fe:develop into MarcelaFiky/winmed-fe:feature/45-education-card
    * Merge branch 'feature/45-education-card' of https://bitbucket.org/MarcelaFiky/winmed-fe into feature/45-education-card
    
    * add card-description height
    
    * Merged creativehandles/winmed-fe into develop
    * Merged creativehandles/winmed-fe into develop
    * Merge branch 'develop' of https://bitbucket.org/MarcelaFiky/winmed-fe into feature/45-education-card
    
    * even-card fix height
    
    * Merged creativehandles/winmed-fe:develop into MarcelaFiky/winmed-fe:feature/45-education-card
    
    Approved-by: Arslan Shahab

---
+++ components/common/event-card/index.tsx
@@ -0,0 +1,49 @@
+import useTranslation from 'next-translate/useTranslation'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import Button from '../button'
+import styles from './EventCard.module.scss'
+
+interface IProps {
+  id: number
+  date: string
+  title: string
+  description: string
+  image: StaticImageData
+}
+
+function EventCard(props: IProps) {
+  const { t } = useTranslation('common')
+  const router = useRouter()
+
+  return (
+    <div className={styles['event-card']}>
+      <Image
+        src={props.image}
+        alt={props.title}
+        layout='responsive'
+        className={styles['event-card-image']}
+        onClick={() => router.push()}
+      />
+      <div className={styles['event-card-description']}>
+        <p className={styles['event-card-date']}>{props.date}</p>
+        <h4 onClick={() => router.push()}>{props.title}</h4>
+        <p className={styles['event-card-desc']}>
+          {props.description?.length > 75
+            ? `${props.description.substring(0, 75)}...`
+            : props.description}
+        </p>
+        <Button
+          size='sm'
+          variant='outlined'
+          type='button'
+          fullWidth
+          onClick={() => router.push()}>
+          {t('moreInfo')}
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default EventCard
